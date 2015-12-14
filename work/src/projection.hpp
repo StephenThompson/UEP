6fa59288d075c44cad400e4889d9a51afc75f33b
Cullum Deighton <cullumdeighton@gmail.com>
2015-12-14 19:12:27 +1300

    Changed everything.

--- work/src/projection.hpp
+++ work/src/projection.hpp
@@ -9,40 +9,31 @@
 class Projection{
 private:
 	std::vector<std::vector<float>> noise_map;
+	std::vector<std::vector<float>> heights;
+	std::vector<std::vector<float>> weights;
 	std::vector<std::vector<std::vector<float>>> noise_map3D;
+	GLuint m_displaylist = 0;
 	int seed;
 	int width;
 	int height;
+
+	float scale;
+	float bumpiness;
+
 	int depth;
+	int trim = 10;
 	float time;
 	float dt = 0.05;
 	float tx=0;
 	float ty=0;
 
-	float interpolation(float, float, float);
-	float cubicInterpolation(float, float, float, float, float);
 
-	float smoothNoise(float);
-	float perlinNoise(float);
-	float noise(int);
-
-	float interpolation2D(float, float, float);
-	float interpolateNoise2D(float,float);
-	float smoothNoise2D(float,float);
-	float perlinNoise2D(float,float);
-	float noise2D(int,int);
-
-	float interpolation3D(float, float, float);
-	float interpolateNoise3D(float,float,float);
-	float smoothNoise3D(float,float,float);
-	float perlinNoise3D(float,float,float);
-	float noise3D(int,int,int);
+	float random();
+	void addHills();
+	void smoothPoints();
 public:
-	Projection(int);
-	void test();
+	Projection();
+	void setWeights(int);
 	void renderLandscape();
-	void testTexture();
-	void setTranslation(float,float);
 	float getHeight(float,float);
-	std::vector<std::vector<float>> getHeightMap();
 };
