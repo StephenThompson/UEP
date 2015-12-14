7fdb78095d740bfbacf8b4ea0fc7afe9b6db89e8
StephenThompson <stephenthomp09@gmail.com>
2015-12-14 18:48:38 +1300

    Fps Counter

--- work/src/main.cpp
+++ work/src/main.cpp
@@ -363,8 +363,22 @@ int main(void)
     	cout<<"Coral "<<i<<endl;
     }
 
+	// fps counter taken from http://www.opengl-tutorial.org/miscellaneous/an-fps-counter/
+	double lastTime = glfwGetTime();
+	int nbFrames = 0;
+
 	while (!glfwWindowShouldClose(window))
 	{
+		// Measure speed
+		double currentTime = glfwGetTime();
+		nbFrames++;
+		if (currentTime - lastTime >= 1.0){ // If last prinf() was more than 1 sec ago
+			// printf and reset timer
+			printf("%f ms/frame\n", 1000.0 / double(nbFrames));
+			nbFrames = 0;
+			lastTime += 1.0;
+		}
+
 		// Cullum
 		//
 		//RENDER CALLS HERE
